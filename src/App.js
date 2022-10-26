bd881c6e1f17927f0713ec11363b661922b5c0b1
unknown <ronan.sibunga@gmail.com>
2022-10-27 02:57:25 +0800

    added contactpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./Pages/Home/Home";
 import NavBar from "../src/Components/NavBar/NavBar";
-import "./App.css";
 import AboutMe from "./Pages/AboutMe/AboutMe";
 import StackSection from "./Pages/StackSection/StackSection";
 import Project from "./Pages/Project/Project";
+import "./App.css";
+import ContactPage from "./Pages/Contact/Contact";
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <AboutMe />
       <StackSection />
       <Project />
-      
+      <ContactPage />
     </div>
   );
 }
